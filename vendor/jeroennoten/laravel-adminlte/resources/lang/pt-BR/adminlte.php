907c315b40d09d0f4ef1fbb50d9f22f386c303c9
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2019-06-27 15:54:06 -0300

    directory vendor

---
+++ vendor/jeroennoten/laravel-adminlte/resources/lang/pt-BR/adminlte.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nome completo',
+    'name'                        => 'Nome',
+    'last_name'                   => 'Sobrenome',
+    'rg'                          => 'RG',
+    'cpf'                         => 'CPF',
+    'email'                       => 'Email',
+    'password'                    => 'Senha',
+    'retype_password'             => 'Repita a senha',
+    'birth_date'                  => 'Data de Nascimento',
+    'sex'                         => 'Sexo',
+    'image'                         => 'Imagem de Perfil',
+    'remember_me'                 => 'Lembrar-me',
+    'register'                    => 'Registrar',
+    'register_a_new_membership'   => 'Registrar um novo membro',
+    'i_forgot_my_password'        => 'Esqueci minha senha',
+    'i_already_have_a_membership' => 'Já sou um membro',
+    'sign_in'                     => 'Entrar',
+    'log_out'                     => 'Sair',
+    'toggle_navigation'           => 'Trocar navegação',
+    'login_message'               => 'Entre para iniciar uma nova sessão',
+    'register_message'            => 'Registrar um novo membro',
+    'password_reset_message'      => 'Recuperar senha',
+    'reset_password'              => 'Recuperar senha',
+    'send_password_reset_link'    => 'Enviar link de recuperação de senha',
+];
