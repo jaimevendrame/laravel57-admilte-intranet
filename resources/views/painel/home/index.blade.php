62460abf84509932dd2070a2177920de9d564cda
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2019-11-19 11:15:24 -0300

    fix home niver

--- resources/views/painel/home/index.blade.php
+++ resources/views/painel/home/index.blade.php
@@ -157,11 +157,11 @@
 {{--                        Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my entire--}}
 {{--                        soul, like these sweet mornings of spring which I enjoy with my whole heart.--}}
 {{--                    </div>--}}
-                    <div class="alert alert-info alert-dismissible ">
+                    <div class="alert alert-info alert-dismissible " style=" display: block;">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                         <h4><i class="icon fa fa-birthday-cake"></i> Aniversariantes do mês!</h4>
                             @forelse($niver as $d)
-                                <div class="row" style=" display: block;">
+                                <div class="row">
                                     <div class="col-md-10 text-truncate">
                                         {{$d->nome_razao." ". $d->sobrenome_fantasia}}
                                     </div>
