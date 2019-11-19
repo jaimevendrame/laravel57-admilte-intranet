eb79e38be670e75ec51de7fd2846835f2c074dc2
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2019-11-19 10:59:07 -0300

    fix home niver

--- resources/views/painel/home/index.blade.php
+++ resources/views/painel/home/index.blade.php
@@ -161,7 +161,7 @@
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                         <h4><i class="icon fa fa-birthday-cake"></i> Aniversariantes do mês!</h4>
                             @forelse($niver as $d)
-                            <p><span class="d-inline-block text-truncate" style="max-width: 200px;">{{$d->nome_razao." ". $d->sobrenome_fantasia}}</span> <span class="pull-right text-bold">{{\Carbon\Carbon::parse($d->birth_date_fundacao)->format('d')}} </span></p>
+                            <div><span class="d-inline-block text-truncate" style="max-width: 200px;">{{$d->nome_razao." ". $d->sobrenome_fantasia}}</span> <span class="pull-right text-bold">{{\Carbon\Carbon::parse($d->birth_date_fundacao)->format('d')}} </span></div>
                             @empty
                                 <p>nenhum registro encontrado! </p>
                             @endforelse
